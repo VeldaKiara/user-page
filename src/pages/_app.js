e9bdfa7fc80ac9e04163517a8449836777384e7b
veldakarimi <veldakarimi@gmail.com>
2023-04-23 16:41:37 +0300

    working user settings page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import NextLink from 'next/link'
+import { SaasProvider, LinkProps  } from '@saas-ui/react'
 
+
+const Link = (props) => {
+  return <NextLink {...props} legacyBehavior />
+}
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SaasProvider linkComponent={Link}>
+        <Component {...pageProps} />
+    </SaasProvider>
+  )
 }
---
+++ src/pages/pwd.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const usePasswordMatch = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isMatch, setIsMatch] = useState(true);
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+    setIsMatch(e.target.value === confirmPassword);
+  };
+
+  const handleConfirmPasswordChange = (
+    e) => {
+    setConfirmPassword(e.target.value);
+    setIsMatch(e.target.value === password);
+  };
+
+  return { password, confirmPassword, isMatch, handlePasswordChange, handleConfirmPasswordChange };
+};
+
+export default usePasswordMatch;
